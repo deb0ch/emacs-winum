8e92e2b7f8359d32d0703ebcd2afde733b62ce7b
deb0ch <thomas.de.beauchene@gmail.com>
2016-12-17 12:58:52 +0100

    update README for Melpa

--- winum.el
+++ winum.el
@@ -1,4 +1,4 @@
-;;; winum.el --- Window numbers: navigate windows and frames using numbers.
+;;; winum.el --- Navigate windows and frames using numbers.
 ;;
 ;; Copyright (c) 2006-2015 Nikolaj Schumacher
 ;; Copyright (c) 2016 Thomas Chauvot de Beauchêne
@@ -33,6 +33,9 @@
 ;; This package is an extended and actively maintained version of the
 ;; https://github.com/nschum/window-numbering.el package by Nikolaj Schumacher,
 ;; with some ideas and code taken from https://github.com/abo-abo/ace-window.
+;;
+;; This version brings, among other things, support for number sets across multiple
+;; frames, giving the user a smoother experience of multi-screen Emacs.
 ;;
 ;;; Code:
 ;;
